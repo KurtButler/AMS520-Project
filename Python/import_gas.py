f34cb23659e83f9bad2f87380aaf8e251b5657d6
Kurt Butler <62613188+guangdongzoo@users.noreply.github.com>
2021-10-28 19:10:19 -0400

    Update import_gas.py

--- Python/import_gas.py
+++ Python/import_gas.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import genfromtxt
+from matplotlib import pyplot as plt
 
 #my_data = genfromtxt('../Data/natgas.data.csv', delimiter=',', 
 #                     dtype="i8,i8,i8,i8,f8,f8,f8,f8,f8,i8");
@@ -8,3 +9,12 @@ from numpy import genfromtxt
 
 my_data = genfromtxt('../Data/natgas.data.csv', delimiter=',',dtype='f8')
 my_data = my_data[1:,]
+
+target = my_data[:,9]
+
+
+plt.title("Matplotlib demo") 
+plt.xlabel("Sample index") 
+plt.ylabel("Next week change in natgas") 
+plt.plot(target) 
+plt.show()
