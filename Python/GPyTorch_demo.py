45be9d189d80bb45f25dbd410cd4f85b39c305c1
Kurt Butler <62613188+guangdongzoo@users.noreply.github.com>
2021-11-17 18:39:20 -0500

    Update GPyTorch_demo.py

--- Python/GPyTorch_demo.py
+++ Python/GPyTorch_demo.py
@@ -7,10 +7,14 @@ from matplotlib import pyplot as plt
 #%load_ext autoreload
 #%autoreload 2
 
+L = 0.05
+
 # Training data is 100 points in [0,1] inclusive regularly spaced
 train_x = torch.linspace(0, 1, 100)
+train_x = torch.cat((train_x.view(-1,1), L*torch.randn(100).view(-1,1)), 1) 
+
 # True function is sin(2*pi*x) with Gaussian noise
-train_y = torch.sin(train_x * (2 * math.pi)) + torch.randn(train_x.size()) * math.sqrt(0.04)
+train_y = torch.sin(train_x[:,0] * (2 * math.pi)) + torch.randn(train_x.size()[0]) * math.sqrt(0.04)
 
 # We will use the simplest form of GP model, exact inference
 class ExactGPModel(gpytorch.models.ExactGP):
@@ -64,6 +68,7 @@ likelihood.eval()
 # Make predictions by feeding model through likelihood
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
     test_x = torch.linspace(0, 1, 51)
+    test_x = torch.cat((test_x.view(-1,1), L*torch.randn(51).view(-1,1)), 1) 
     observed_pred = likelihood(model(test_x))
 
 
@@ -75,11 +80,11 @@ with torch.no_grad():
     # Get upper and lower confidence bounds
     lower, upper = observed_pred.confidence_region()
     # Plot training data as black stars
-    ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+    ax.plot(train_x.numpy()[:,0], train_y.numpy(), 'k*')
     # Plot predictive means as blue line
-    ax.plot(test_x.numpy(), observed_pred.mean.numpy(), 'b')
+    ax.plot(test_x.numpy()[:,0], observed_pred.mean.numpy(), 'b')
     # Shade between the lower and upper confidence bounds
-    ax.fill_between(test_x.numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
+    ax.fill_between(test_x.numpy()[:,0], lower.numpy(), upper.numpy(), alpha=0.5)
     ax.set_ylim([-3, 3])
     ax.legend(['Observed Data', 'Mean', 'Confidence'])
 
